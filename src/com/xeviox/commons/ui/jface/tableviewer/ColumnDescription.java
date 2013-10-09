c5fd3c06dfdcce47fdfcfc666e452839eac6c323
Tobi <erebado@gmail.com>
2013-10-09 16:37:13 +0200

    add license header

--- src/com/xeviox/commons/ui/jface/tableviewer/ColumnDescription.java
+++ src/com/xeviox/commons/ui/jface/tableviewer/ColumnDescription.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Benjamin Pabst and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Benjamin Pabst - initial API and implementation
+ *    Tobias Placht
+ ******************************************************************************/
 package com.xeviox.commons.ui.jface.tableviewer;
 
 import java.util.Comparator;
